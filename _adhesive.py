61b7a05d2f5aa70bc145c905e38261019a7af6d3
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-11-25 22:49:46 +0100

    Handle builds by adhesive as well.

--- _adhesive.py
+++ _adhesive.py
@@ -2,7 +2,7 @@ from gbs_pipeline import pipeline_build_gbs_images
 
 
 pipeline_build_gbs_images({
-    "base_containers": {
+    "build_containers": {
         "ubuntu-18.04": "germaniumhq/ubuntu:18.04",
     }
 })
